6e34f855b4e57f59ccf55fb1245df18300326cfb
AliTariqNagi <97218740+AliTariqNagi@users.noreply.github.com>
2022-01-10 18:05:49 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-function sayHello(MyName) {
+/*function sayHello(MyName) {
     console.log('Hello' +   ' '   + MyName);
 }
 
 sayHello('Ali Tariq');
+"""
+*/
+
+var log = require('./logger.js');
+
+const logger2 = require('./logger.js');
+
+//console.log(logger);
+
+//logger.log('Hello');
+//logger2.log('second object of logger');
+
+log('Function exported only')
--- logger.js
+++ logger.js
@@ -1,8 +1,13 @@
+console.log(__filename)
+console.log(__dirname)
+
 var url  = 'http://mylogger.io/log';
 
 function log(message) {
 console.log(message);
 }
 
-module.exports.log = log;
-module.exports.endPoint = url;
+//module.exports.log = log;
+//module.exports.endPoint = url;
+
+module.exports = log
